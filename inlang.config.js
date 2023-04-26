07f7e64cb309d611db24a3931c7471684c9850ef
Niklas Buchfink <59048346+NiklasBuchfink@users.noreply.github.com>
2023-04-26 14:48:48 +0200

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -5,37 +5,24 @@
  */
 export async function defineConfig(env) {
   const plugin = await env.$import(
-      "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
   );
 
-  const {
-      standardLintRules
-  } = await env.$import(
-      "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
+  const { standardLintRules } = await env.$import(
+    "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
   );
 
   const pluginConfig = {
-      pathPattern: "./src/data/languages/{language}.json",
+    pathPattern: "./src/data/languages/{language}.json",
   };
 
   return {
-      referenceLanguage: "English",
-      languages: await plugin.getLanguages({
-          ...env,
-          pluginConfig
-      }),
-      readResources: (args) => plugin.readResources({
-          ...args,
-          ...env,
-          pluginConfig
-      }),
-      writeResources: (args) => plugin.writeResources({
-          ...args,
-          ...env,
-          pluginConfig
-      }),
-      lint: {
-          rules: [standardLintRules()],
-      },
+    referenceLanguage: "en",
+    languages: await plugin.getLanguages({ ...env, pluginConfig }),
+    readResources: (args) => plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) => plugin.writeResources({ ...args, ...env, pluginConfig }),
+    lint: {
+      rules: [standardLintRules()],
+    },
   };
 }
